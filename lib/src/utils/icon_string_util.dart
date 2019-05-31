b29a5e37190a2c293dd47cbdd107a63dfcb844f1
Mariano <marianolop22@yahoo.com.ar>
2019-05-31 08:53:12 -0300

    avance

--- lib/src/utils/icon_string_util.dart
+++ lib/src/utils/icon_string_util.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 final _icons = <String, IconData>{
   'add_alert' : Icons.add_alert,
   'accessibility' : Icons.accessibility,
-  'folder_open' : Icons.add_alert
+  'folder_open' : Icons.add_alert,
+  'donut_large': Icons.donut_large
 };
 
 Icon getIcon ( String iconName ) {
